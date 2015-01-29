f29ff0eba655352bce131151df781f22007820d6
Edward Wu <Edward@128.189.224.76>
2015-01-29 15:14:41 -0800

    Deleting something in LoginServices

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/LoginService.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/LoginService.java
@@ -3,9 +3,7 @@ package com.google.gwt.sample.stockwatcher.client;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-@RemoteServiceRelativePath("login")
-public interface LoginService extends RemoteService {
-  public LoginInfo login(String requestUri);
-}
+
+Public void THERE WILL BE AN ROOEEAF
 
 public void haha throws void NullPointerException(){}
