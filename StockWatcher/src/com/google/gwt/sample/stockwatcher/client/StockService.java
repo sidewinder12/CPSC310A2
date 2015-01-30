d5e906325228dd2f1df6be9c8bef77a390f1e01e
Edward Wu <Edward@128.189.224.76>
2015-01-29 16:02:39 -0800

    aa branch created

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockService.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockService.java
@@ -5,8 +5,12 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("stock")
 public interface StockService extends RemoteService {
-  public void addStock(String symbol) throws NotLoggedInException;
+	public String[] getStocks() throws NotLoggedInException;
+	public String[] getStocks() throws NotLoggedInException;
+	public String[] getStocks() throws NotLoggedInException;
+	public String[] getStocks() throws NotLoggedInException;
   public void removeStock(String symbol) throws NotLoggedInException;
   public String[] getStocks() throws NotLoggedInException;
+  fak;fkafadaaadfda;ao;a
 }
 
